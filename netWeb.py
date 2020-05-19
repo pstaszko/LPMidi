a4f310c18a4f3dce1cc2f66a3d96a912320de208
Paul Staszko <paulstaszko@gmail.com>
2020-05-19 16:38:58 -0500

    quick

--- netWeb.py
+++ netWeb.py
@@ -19,11 +19,13 @@ def hello_world():
         red=f.get('red', default = 0, type = int)
         green=f.get('green', default = 0, type = int)
         blue=f.get('blue', default = 0, type = int)
-        if cmd=='clear':
-            pad.reset()
+        if cmd=='clear': pad.reset()
+        elif cmd=='reset': pad.reset()
         elif cmd=='set_led_xy_by_colour': pad.set_led_xy_by_colour(x, y, pad.colours['white'])
         elif cmd=='set_led_xy': pad.set_led_xy(x, y, red, green, blue)
         elif cmd=='set_all_on': pad.set_all_on(red, green, blue)
+        elif cmd=='paint_app': pad.paint_app()
+        elif cmd=='set_all_on_slow': pad.paint_app(red, green, blue)
         
         
         return cmd
